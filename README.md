bfa705a94ebea5417a13af9b256093479d7fc483
oskar117 <olek.konieczny@wp.pl>
2019-10-16 16:30:30 +0200

    test2

---
+++ README.md
@@ -0,0 +1,8 @@
+Reverse Proxy - działa po stronie serwera, jest pośrednikiem dzięki którym
+	dowonly klient może połączyć się z serwerem.
+Forward proxy - jest pośrednikie, który pozwala klientom połączyć się z 
+	każdym serwerem.
+
+Nginx pozwala na postawienie serwera reverse proxy, dzięki czemu klient z
+	zewnątrz może połączyć się z serwere wildfly, bez udostępniania tego 
+	serwera publicznie.
--- app/src/main/java/pl/edu/pjwstk/jazapp/webapp/LoginController.java
+++ app/src/main/java/pl/edu/pjwstk/jazapp/webapp/LoginController.java
@@ -1,4 +1,18 @@
 package pl.edu.pjwstk.jazapp.webapp;
 
+import pl.edu.pjwstk.jazapp.login.LoginRequest;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
 public class LoginController {
+    @Inject
+    private LoginRequest loginRequest;
+
+    public void login() {
+        System.out.println("Tried to log in using " + loginRequest.toString());
+    }
 }
